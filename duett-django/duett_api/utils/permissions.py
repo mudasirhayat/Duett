3f9ea732b6456b695f6d2d7e1beb6b24bf23da45
mudasirhayat <mudasirhayat5@gmail.com>
2025-08-06 05:00:06 +0000

    fix: improve error handling

--- duett-django/duett_api/utils/permissions.py
+++ duett-django/duett_api/utils/permissions.py
@@ -3,8 +3,8 @@ from rest_framework import permissions
 
 class ObjectPermission(permissions.BasePermission):
     """
-    Checks that user is object owner
-    """
-
-    def has_permission(self, request, view, obj):
-        return obj.id == request.user
+def has_permission(self, request, view, obj):
+    try:
+        return obj.id == request.user.id
+    except AttributeError:
+        return False
