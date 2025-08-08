9d4effe4f2e147738c34ff1a90dfbd72dae25b0e
mudasirhayat <mudasirhayat5@gmail.com>
2025-08-08 13:00:04 +0000

    fix: improve error handling

--- duett-react/src/views/Page503.js
+++ duett-react/src/views/Page503.js
@@ -3,6 +3,10 @@ import { Box } from '@material-ui/core';
 import FlexBox from '../components/layout/FlexBox';
 
 const Page503 = () => {
+  try {
+    // Code that may throw an error
+  } catch (error) {
+    console.error
   return (
     <Box px={4} py={2} width="100%">
       <FlexBox justifyContent="center" mt={4}>
