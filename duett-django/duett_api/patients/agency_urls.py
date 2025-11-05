f273f4c7467795701fa197acfff76097a458e3c5
mudasirhayat <mudasirhayat5@gmail.com>
2025-11-05 13:00:06 +0000

    fix: improve error handling

--- duett-django/duett_api/patients/agency_urls.py
+++ duett-django/duett_api/patients/agency_urls.py
@@ -1,8 +1,9 @@
 
 from django.urls import path, include
-from .views import CareRequestGetApiView,ClientGetApiView,ClientHistoryGetAPIView
+from .views import CareRequestGetApiView, ClientGetApiView, ClientHistoryGetAPIView
 
 urlpatterns = [
+    path('care-request/', CareRequestGetApiView.as_view(), name='care-request'),
     path("client/<int:user_id>/",ClientGetApiView.as_view(),name="client-search"),
     path("carerequest/<int:user_id>/",CareRequestGetApiView.as_view(),name="care-request-search"),
     path("clienthistory/<int:user_id>/",ClientHistoryGetAPIView.as_view(),name='client-history-search'),
