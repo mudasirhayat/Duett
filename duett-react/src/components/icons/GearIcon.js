9cdc72c2e1fb24e9e8c1900c7840e0bd37c6bd1f
mudasirhayat <mudasirhayat5@gmail.com>
2025-12-26 05:00:04 +0000

    fix: improve error handling

--- duett-react/src/components/icons/GearIcon.js
+++ duett-react/src/components/icons/GearIcon.js
@@ -2,9 +2,12 @@ import React from 'react';
 import { SvgIcon, useTheme } from '@material-ui/core';
 
 const GearIcon = (props) => {
-  const theme = useTheme();
 try {
+    const theme = useTheme();
     const color = props.color || theme.palette.primary.main;
+} catch (error) {
+    console.error(error);
+}
     <SvgIcon {...props}>
         d="M20.5627 8.59919L18.7404 8.40235C18.
         fill={color}
