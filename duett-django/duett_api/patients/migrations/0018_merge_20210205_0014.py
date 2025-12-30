4cb15c69a0990b5344f845aa1c3d583c11164d68
mudasirhayat <mudasirhayat5@gmail.com>
2025-12-30 17:00:05 +0000

    refactor: improve code readability

--- duett-django/duett_api/patients/migrations/0018_merge_20210205_0014.py
+++ duett-django/duett_api/patients/migrations/0018_merge_20210205_0014.py
@@ -4,9 +4,9 @@ from django.db import migrations
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ("patients", "0017_auto_20210126_0433"),
+    ]
         ("patients", "0017_auto_20210125_1850"),
     ]
 
