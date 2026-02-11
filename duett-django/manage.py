5eadb0145486c16154e08695a4a3afe51eed140c
mudasirhayat <mudasirhayat5@gmail.com>
2026-02-11 09:00:05 +0000

    fix: improve error handling

--- duett-django/manage.py
+++ duett-django/manage.py
@@ -6,7 +6,10 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+    try:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+    except Exception as e:
+        print(f"An error occurred: {e}")
     try:
         from django.core.management import execute_from_command_line
 except ImportError as exc:
