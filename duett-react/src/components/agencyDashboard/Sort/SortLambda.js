dbe7c7c781a9660dca462abf700327bfdd1ed0cd
mudasirhayat <mudasirhayat5@gmail.com>
2026-02-27 05:00:04 +0000

    refactor: improve code readability

--- duett-react/src/components/agencyDashboard/Sort/SortLambda.js
+++ duett-react/src/components/agencyDashboard/Sort/SortLambda.js
@@ -1,8 +1,8 @@
 import { getServiceHours } from '../../../lib/helpers';
 
 export const sortLambda = {
-  id: (u) => u.id,
-  clientName: (u) => u.patient.name.toLowerCase(),
+id: (user) => user.id,
+  clientName: (user) => user.patient.name.toLowerCase(),
   zipCode: (u) => u.patient.zip,
   hours: (u) => getServiceHours(u.services[0].frequency, u.services[0].hours),
   lastRefreshed: (u) => new Date(u.refreshed_time),
