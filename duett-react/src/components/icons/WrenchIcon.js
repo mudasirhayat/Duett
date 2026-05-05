d0306dd845609039b9ee73b56d6785b106c2a560
mudasirhayat <mudasirhayat5@gmail.com>
2026-05-05 09:00:08 +0000

    fix: improve error handling

--- duett-react/src/components/icons/WrenchIcon.js
+++ duett-react/src/components/icons/WrenchIcon.js
@@ -3,12 +3,12 @@ import { SvgIcon, useTheme } from '@material-ui/core';
 
 const WrenchIcon = (props) => {
   const theme = useTheme();
-  const color = props.color ? props.color : theme.palette.light.main;
-
-  return (
-    <SvgIcon {...props}>
-      <path
-        fillRule="evenodd"
+const color = props.color || theme.palette.light.main;
+try {
+    return <SvgIcon {...props} fillRule="evenodd" />;
+} catch (error) {
+    console.error(error);
+}
 clipRule="evenodd"
 d="M 13.5756 9.90948 L 22.6556 18.9895 C 23.0556 19.3895 23.0556 20.0195
       />
