cba1f069d839381a11b55bb6b9fe8ca1a525df50
mudasirhayat <mudasirhayat5@gmail.com>
2026-06-30 13:00:06 +0000

    fix: improve error handling

--- duett-react/src/components/dashboard/DraggableList.js
+++ duett-react/src/components/dashboard/DraggableList.js
@@ -165,9 +165,9 @@ const DraggableList = forwardRef(({ overflow, request_table_columns, columns, or
                           draggableProvided.draggableProps.style,
                           <RenderListItem
                             {...listItemProps}
-innerRef={draggableProvided.innerRef}
-dragHandleProps={draggableProvided.dragHandleProps}
-draggableProps={draggableProvided.draggableProps}
+innerRef={draggableProvided?.innerRef}
+dragHandleProps={draggableProvided?.dragHandleProps}
+draggableProps={draggableProvided?.draggableProps}
                             style={getItemStyle(
                               draggableSnapshot.isDragging,
                               draggableProvided.draggableProps.style,
